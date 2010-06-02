9fd4c641cdf7719463c1ce31788034c43d23ef2f
Ray Hernandez <hernan43@gmail.com>
2010-06-02 17:44:13 -0400

    Initial import of a working devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Phodown::Application.routes.draw do |map|
+  devise_for :users
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "base#index"
