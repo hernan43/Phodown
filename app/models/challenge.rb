e0118661d102bdb9c319723d680d0436a78e6000
Ray Hernandez <hernan43@gmail.com>
2010-06-02 19:27:25 -0400

    Setting up scopes

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,4 +2,7 @@ class Challenge < ActiveRecord::Base
   belongs_to :user
   belongs_to :type
   belongs_to :rule
+  
+  scope :active, where(:is_hidden => false).order("name")
+  scope :open,  proc { where("start < ?", DateTime.now).where("end > ?", DateTime.now)}
 end
