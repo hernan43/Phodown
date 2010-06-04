eef08c831ebac082a05e87af3f83fdcd011d1d4e
Ray Hernandez <hernan43@gmail.com>
2010-06-03 21:31:43 -0400

    EXIF data is working

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,7 @@ class Entry < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
 
-  has_attached_file :photo, :styles => { :medium => ["800x800>", :jpg], :thumb => ["128x128>", :jpg] }
+  has_attached_file :photo, :styles => { :original => ["800x800>", :jpg], :thumb => ["128x128>", :jpg] }
 
   scope :active, where(:is_hidden => false).order("name")
 
@@ -15,11 +15,47 @@ class Entry < ActiveRecord::Base
     attachment_for(:photo).assign(file)
   end
 
-
   protected
   
+  def string_fraction_to_float(fraction)
+    return nil if fraction.nil?
+    begin
+      /(\d+)\/(\d+)/.match(fraction)
+      $1.to_f / $2.to_f
+    rescue
+      nil
+    end
+  end
+
+  def split_exif(exif_data)
+      exif_entries = exif_data.split("\n")
+      exif_entries = exif_entries.collect do |entry|
+        begin 
+          entry.split(":")[1].strip.split("=")
+        rescue
+        end
+      end
+
+      # for the life of me I can't find a better way of doing this
+      exif_hash = {}
+      exif_entries.each do |entry|
+        begin
+          exif_hash[entry[0].to_sym] = entry[1]
+        rescue
+        end
+      end
+      return exif_hash
+  end
+
   def set_exif_data(file)
     self.exif = `identify -format "%[exif:*]" #{file.path}`
+    exif = split_exif(self.exif)
+    self.shutter_speed = exif[:ExposureTime] if not exif[:ExposureTime].nil?
+    self.camera_model = exif[:Model] if not exif[:Model].nil?
+    self.camera_make = exif[:Make] if not exif[:Make].nil?
+    self.iso = exif[:ISOSpeedRatings] if not exif[:ISOSpeedRatings].nil?
+    self.flash = exif[:Flash] if not exif[:Flash].nil?
+    self.aperture = string_fraction_to_float(exif[:ApertureValue])
   end
 
   def set_average_rating(rating=0.0)
