98cc22b3d7cc4a73495c13069475ca8a5e5bcabe
Ray Hernandez <hernan43@gmail.com>
2010-06-08 19:04:16 -0400

    Stubbing out AJAX rating system

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,9 +1,9 @@
 module EntriesHelper
 
   def rating_link(entry, value, user)
-    html_options = {}
+    html_options = { 'data-remote' => 'true'}
     html_options[:class] = "selected_rating" if entry.rating_for(user).value == value
-    link_to(value, rate_challenge_entry_path(@challenge, @entry, {:value => value}, html_options)).html_safe
+    link_to(value, rate_challenge_entry_path(@challenge, @entry, {:value => value}), html_options)
   end
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+
+  // remote links handler
+  $('a[data-remote=true]').live('click', function() {
+    return request({ url : this.href });
+  });
+
+});
