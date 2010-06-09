59955428edd84e7528ca624bc3178bfb66f17d6d
Ray Hernandez <hernan43@gmail.com>
2010-06-08 22:09:10 -0400

    Comments and gravatars are working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def avatar_for(user)
+    # include MD5 gem, should be part of standard ruby install
+    require 'digest/md5'
+
+    # get the email from URL-parameters or what have you and make lowercase
+    email_address = user.email.downcase
+
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+
+    # compile URL which can be used in <img src=""...
+    image_src = "<img src=\"http://www.gravatar.com/avatar/#{hash}\"/>".html_safe
+  end
 end
