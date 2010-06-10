5b4fb817622a52db5ff0b54363120b22e221e291
Ray Hernandez <hernan43@gmail.com>
2010-06-10 12:12:00 -0400

    Added some scopes for the challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -8,6 +8,7 @@ class Challenge < ActiveRecord::Base
   scope :active, where(:is_hidden => false).order("name")
   scope :open,  proc { where("start < ?", DateTime.now).where("end > ?", DateTime.now).order("end ASC")}
   scope :voting,  proc { where("end < ?", DateTime.now).where("end > ?", DateTime.now - 7.days).order("end ASC")}
+  scope :recent,  proc { where("end < ?", DateTime.now).where("end < ?", DateTime.now - 7.days).order("end ASC").limit(5)}
 
   def voting_end_date
     self.end + 7.days
